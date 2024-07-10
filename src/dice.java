8045cbbe56a1dcab94c39d7357bde79a680720a0
vishal2970 <vishalagraharibasti@gmail.com>
2024-07-10 09:17:36 +0530

    Created Dice class

--- src/dice.java
+++ src/dice.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class dice {
+import java.util.Random;
+
+public class dice {
+    private Random random;
+    private int sides;
+
+    public dice(int sides) {
+        this.sides = sides;
+        this.random = new Random();
+    }
+
+    public int roll() {
+        return random.nextInt(sides) + 1;
+    }
 }
