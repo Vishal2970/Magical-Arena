7406f44c6a1cb13e22f291f616df5cf1c0244a24
vishal2970 <vishalagraharibasti@gmail.com>
2024-07-10 09:12:51 +0530

    Created Player class

---
+++ src/player.java
@@ -0,0 +1,31 @@
+public class player {
+    private int health;
+    private int strength;
+    private int attack;
+
+    public player(int health, int strength, int attack) {
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void reduceHealth(int damage) {
+        this.health -= damage;
+    }
+
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+}
